b173ef91f943e9dd1d813870fcce0f216c6e459a
Emir <emir.gradjanin@gmail.com>
2016-12-20 22:51:23 +0100

    Marking todos incomplete

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -4,9 +4,10 @@ feature "User completes todo" do
   scenario "successfully" do
     sign_in
 
-     create_todo "Buy milk"
-     click_on "Mark complete"
+    create_todo "Buy milk"
 
-     display_completed_todo "Buy milk"
+    click_on "Mark complete"
+
+    expect(page).to  display_completed_todo "Buy milk"
   end
 end
